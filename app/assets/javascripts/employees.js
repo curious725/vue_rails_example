b39001f956c6ef3bba060a6279322debb657904f
curious725 <curious725@gmail.com>
2018-04-13 17:52:10 +0300

    include script for Vue.js

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,3 +1,16 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var employees = new Vue({
+  el: '#employees',
+  data: {
+    employees: []
+  },
+  ready: function() {
+    var that;
+    that = this;
+    $.ajax({
+      url: '/employees.json',
+      success: function(res) {
+        that.employees = res;
+      }
+    })
+  }
+})
